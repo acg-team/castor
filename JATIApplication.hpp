e8c7c181b16558420413740a1ae04d69f27a4f03
Lorenzo Gatti <lorenzo.gatti.89@gmail.com>
2018-03-05 19:08:13 +0100

    #ADD: Added help and banner for the command line
    Signed-off-by: Lorenzo Gatti <lorenzo.gatti.89@gmail.com>

--- JATIApplication.hpp
+++ JATIApplication.hpp
@@ -47,6 +47,8 @@
 #include <string>
 #include <map>
 #include <Bpp/Exceptions.h>
+#include <glog/logging.h>
+#include <iostream>
 
 namespace bpp {
     class JATIApplication {
@@ -72,7 +74,63 @@ namespace bpp {
 
         std::string &getParam(const std::string &name) { return params_[name]; }
 
+
+        void help() {
+            std::cout << appName_ << std::endl << std::endl;
+            std::cout << "Usage: miniJATI [options]" << std::endl;
+            std::cout << std::endl << "**** Input options ****" << std::endl << std::endl;
+            std::cout << "\talphabet={DNA,RNA,Protein}      Dataset alphabet     [requested]\t(inherited from bpp documentation)" << std::endl;
+            std::cout << "\tinput_sequences=<filepath>                           [requested]" << std::endl;
+            std::cout << "\tinput_tree=<filepath>                                [not-requested]" << std::endl;
+
+            std::cout << std::endl << "**** Alignment options ****" << std::endl << std::endl;
+            std::cout << "\talignment=<bool>                                     [requested]" << std::endl;
+
+            std::cout << std::endl << "**** Substitution model options ****" << std::endl << std::endl;
+            std::cout << "\tmodel_substitution=<string>                          [requested]\t(inherited from bpp documentation)" << std::endl;
+            std::cout << "\tmodel_setfreqsfromdata=<bool>                        [requested]\t(inherited from bpp documentation)" << std::endl;
+            std::cout << "\tmodel_pip_lambda=<float>                             (if models_indels=true)" << std::endl;
+            std::cout << "\tmodel_pip_mu=<float>                                 (if models_indels=true)" << std::endl;
+
+            std::cout << std::endl << "**** Likelihood computation options ****" << std::endl << std::endl;
+            std::cout << "\tlkmove={single,double}                               [not-requested]" << std::endl;
+
+            std::cout << std::endl << "**** Parameter optimisation options ****" << std::endl << std::endl;
+            std::cout << "\toptimization=<string>                                        [requested]\t(inherited from bpp documentation)" << std::endl;
+            std::cout << "\toptimization.message_handler=<filepath>                      [not-requested]\t(inherited from bpp documentation)" << std::endl;
+            std::cout << "\toptimization.profiler=<filepath>                             [not-requested]\t(inherited from bpp documentation)" << std::endl;
+            std::cout << "\toptimization.reparametrization=<bool>                        [not-requested]\t(inherited from bpp documentation)" << std::endl;
+            std::cout << "\toptimization.max_number_f_eval=<int>                         [not-requested]\t(inherited from bpp documentation)" << std::endl;
+            std::cout << "\toptimization.tolerance=<float>                               [not-requested]\t(inherited from bpp documentation)" << std::endl;
+            std::cout << "\toptim_topology_algorithm={none,greedy,hillclimbing,swarm}    [requested]" << std::endl;
+            std::cout << "\toptim_topology_numnodes=<int>                                number of random nodes to use during the hillclimbing" << std::endl;
+            std::cout << "\toptim_topology_operations={nni-search,spr-search,tbr-search,best-search}" << std::endl;
+            std::cout << "\toptim_topology_maxcycles=<int>" << std::endl;
+
+            std::cout << std::endl << "Verbosity:" << std::endl;
+            std::cout << "\texport GLOG_v={0,1,2,3}" << std::endl << "\texport GLOG_minloglevel={INFO,FATAL,WARN}" << std::endl << std::endl;
+            std::cout << "Examples:" << std::endl;
+            std::cout << "Documentation can be found at https://bitbucket.org/acg-team/minijati/" << std::endl;
+        }
+
+
+        void banner() {
+
+            LOG(INFO) << "*****************************************************************************************************************************************";
+            LOG(INFO) << "* " << appName_ << "  *";
+            LOG(INFO) << "* Authors: Lorenzo Gatti & Massimo Maiolo                                                                                               *";
+            LOG(INFO) << "* ------------------------------------------------------------------------------------------------------------------------------------- *";
+            LOG(INFO) << "* Based on Bio++ by J. Dutheil, B. Boussau, L. Guéguen, M. Groussin                                                                     *";
+            LOG(INFO) << "* Inspired on codonPhyML (Gil M. et al.)                                                                                                *";
+            LOG(INFO) << "* Inspired on PrographMSA (Szalkowski A. et al.)                                                                                        *";
+            LOG(INFO) << "* Implements the Poisson Indel Model (Bouchard-Cote A. et al.)                                                                          *";
+            LOG(INFO) << "*****************************************************************************************************************************************";
+
+
+        }
+
     };
+
 
 } //end of namespace bpp;
 
--- README.md
+++ README.md
@@ -4,8 +4,10 @@
 
 ### Usage
 
-./miniJATI alphabet=DNA alignment=false input_sequences=../data/test_treesearch/sim_dna_5.fa lkmove=bothways optimization=D-Brent(derivatives=Newton) optimization
-.message_handler=../out_opt.txt optimization.profiler=../out_opt_prof.txt optimization.reparametrization=false optimization.max_number_f_eval=1000 optimization.tolerance=0.001 optim_topology_algorithm=greedy optim_topology_numnodes=4 optim_topology_operations=best-search optim_topology_maxcycles=100 model_substitution=HKY85 model_indels=true model_setfreqsfromdata=false model_pip_lambda=0.2 model_pip_mu=0.1
+    ./miniJATI alphabet=DNA alignment=false input_sequences=../data/test_treesearch/sim_dna_5.fa lkmove=bothways optimization=D-Brent(derivatives=Newton) optimization
+    .message_handler=../out_opt.txt optimization.profiler=../out_opt_prof.txt optimization.reparametrization=false optimization.max_number_f_eval=1000 optimization.tolerance=0.001 optim_topology_algorithm=greedy optim_topology_numnodes=4 optim_topology_operations=best-search optim_topology_maxcycles=100 model_substitution=HKY85 model_indels=true model_setfreqsfromdata=false model_pip_lambda=0.2 model_pip_mu=0.1
+
+
 
 
 ## Verbose options
@@ -15,18 +17,18 @@
 
 ### Arguments
 
-    alphabet={DNA,RNA,Protein}                      (inherited from bpp documentation)  [requested]
-    alignment={true,false}                                                              [requested]  
-    input_sequences=path/to/sequences.fa                                                [requested]     
-    input_tree=path/to/tree.nwk                                                         [not-requested] 
-    lkmove="bothways"                                                                   [not-requested]
-    optimization=D-Brent(derivatives=Newton)        (inherited from bpp documentation)  [requested]
-    optimization.message_handler=../out_opt.txt     (inherited from bpp documentation)  [not-requested]
-    optimization.profiler=../out_opt_prof.txt       (inherited from bpp documentation)  [not-requested]
-    optimization.reparametrization=false            (inherited from bpp documentation)  [not-requested]
-    optimization.max_number_f_eval=1000             (inherited from bpp documentation)  [not-requested]
-    optimization.tolerance=0.001                    (inherited from bpp documentation)  [not-requested]
-    optim_topology_algorithm="greedy"               {none, greedy, hillclimbing}        [requested]
+    alphabet={DNA,RNA,Protein}                      (inherited from bpp documentation)      [requested]
+    alignment={true,false}                                                                  [requested]  
+    input_sequences=path/to/sequences.fa                                                    [requested]     
+    input_tree=path/to/tree.nwk                                                             [not-requested] 
+    lkmove="bothways"                                                                       [not-requested]
+    optimization=D-Brent(derivatives=Newton)        (inherited from bpp documentation)      [requested]
+    optimization.message_handler=../out_opt.txt     (inherited from bpp documentation)      [not-requested]
+    optimization.profiler=../out_opt_prof.txt       (inherited from bpp documentation)      [not-requested]
+    optimization.reparametrization=false            (inherited from bpp documentation)      [not-requested]
+    optimization.max_number_f_eval=1000             (inherited from bpp documentation)      [not-requested]
+    optimization.tolerance=0.001                    (inherited from bpp documentation)      [not-requested]
+    optim_topology_algorithm="greedy"               {none, greedy, hillclimbing, swarm}     [requested]
     optim_topology_numnodes=4                       number of random nodes to use during the hillclimbing 
     optim_topology_operations="best-search"         {nni-search, spr-search, tbr-search, best-search}    
     optim_topology_maxcycles=100
