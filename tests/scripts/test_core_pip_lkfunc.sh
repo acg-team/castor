2075e03564440a172e71260dc8bceb5206147386
Lorenzo Gatti <lorenzo.gatti.89@gmail.com>
2018-05-30 10:14:24 +0200

    #FIX: ofc, bc does not exist on the docker image. Opted to use an inline function with AWK for comparing floating numbers in bash
    Signed-off-by: Lorenzo Gatti <lorenzo.gatti.89@gmail.com>

--- tests/scripts/test_core_pip_lkfunc.sh
+++ tests/scripts/test_core_pip_lkfunc.sh
@@ -2,9 +2,10 @@
 
 var1=$(cat ./out_core_pip_lkfunc.estimates.json | grep LogLikelihood | awk -F":" '{ print $2}' | sed 's/\"//g' | sed 's/ //g');
 var2=-21.354418417018628;
-status=$(echo $var1'=='$var2 | bc -l);
+#status=$(echo $var1'=='$var2 | bc -l);
+status=$(awk "function abs(value){return (value<0?-value:value);}; BEGIN {print (abs($var1)-abs($var2))}");
 
-if [ "$status" -eq "1" ]; then
+if [ "$status" -eq "0" ]; then
    exit 0;
 else
    exit 1;
