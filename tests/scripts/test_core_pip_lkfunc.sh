09b3f56dacff75e6651a3df48f2b9753a75cd0a1
Lorenzo Gatti <lorenzo.gatti.89@gmail.com>
2018-06-22 15:17:24 +0200

    #FIX: Fixed CORE PIP LKFUNC value
    Signed-off-by: Lorenzo Gatti <lorenzo.gatti.89@gmail.com>

--- tests/scripts/test_core_pip_lkfunc.sh
+++ tests/scripts/test_core_pip_lkfunc.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 var1=$(cat ./out_core_pip_lkfunc.estimates.json | grep LogLikelihood | awk -F":" '{ print $2}' | sed 's/\"//g' | sed 's/ //g');
-var2=-21.358154531926619;
+var2=-21.3581545319266;
 #status=$(echo $var1'=='$var2 | bc -l);
 status=$(awk "function abs(value){return (value<0?-value:value);}; BEGIN {print (abs($var1)-abs($var2))}");
 
